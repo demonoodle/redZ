d16551a2848944b76228bd6c750af5891c0af50e
demonoodle <irvanrp@gmail.com>
2018-12-18 20:52:27 +0700

    a

--- commands/info.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'info',
-	description: 'Display info about yourself.',
-	execute(message) {
-		message.channel.send(`Username: ${message.author.username}\nID: ${message.author.id}`);
-	},
-};
